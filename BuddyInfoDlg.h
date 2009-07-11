d33ebf48fa08e3aeaf05965d5351f90d468fdd5e
shikun.z <shikun.z@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-07-11 21:09:56 +0000

    添加好友信息查看(部分功能完成) 移动信息提示的几个方法到CNotify类中,以便管理 完善了主窗口的右键菜单,功能有待完善

---
+++ BuddyInfoDlg.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "afxwin.h"
+
+
+// CBuddyInfoDlg 对话框
+
+class CBuddyInfoDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CBuddyInfoDlg)
+
+public:
+	CBuddyInfoDlg(long lAccountID = 0, CWnd* pParent = NULL);   // 标准构造函数
+	
+	virtual ~CBuddyInfoDlg();
+
+// 对话框数据
+	enum { IDD = IDD_WMLF_BUDDYINFO };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	CCommandBar m_dlgCommandBar;
+
+	DECLARE_MESSAGE_MAP()
+public:
+
+
+    virtual BOOL OnInitDialog();
+    afx_msg void OnSize(UINT nType, int cx, int cy);
+    void CBuddyInfoDlg::InitScrollInfo();
+
+	const Fetion_Account *m_account;
+    // 屏显名称
+    CString m_strShowName;
+    // 所属组
+    int m_iGroupID;
+    // 飞信号码
+    long m_lAccountID;
+    // 昵称
+    CString m_strNickName;
+    CString m_strName;
+    CString m_strSex;
+    CString m_strProv;
+    CString m_strCity;
+    // 签名
+    CString m_strSign;
+    void InitGroupItem(void);
+    CComboBox m_cboGroup;
+    afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+};
--- Notify.cpp
+++ Notify.cpp
@@ -1,6 +1,70 @@
 #include "StdAfx.h"
 #include "Notify.h"
 
+#ifdef WIN32_PLATFORM_WFSP
+#include "vibrate.h"
+#endif
+
+#ifdef WIN32_PLATFORM_PSPC
+#include <Nled.h>
+#if _WIN32_WCE<0x500
+extern "C"
+{
+	BOOL NLedGetDeviceInfo(INT nID, PVOID pOutput);
+	BOOL NLedSetDevice(INT nID, PVOID pOutput);
+}
+#endif
+#endif
+
+#define TIMER_STOPVIBRATE 30
+
+#ifdef WIN32_PLATFORM_WFSP
+//SP用于停止振动
+void CALLBACK StopVib(
+					  HWND hwnd, 
+					  UINT uMsg, 
+					  UINT idEvent, 
+					  DWORD dwTime 
+					  )
+{
+	//停止振动
+	VibrateStop();
+}
+#endif
+
+
+#ifdef WIN32_PLATFORM_PSPC
+//PPC振动函数
+void StartVirbate( )
+{
+	NLED_SETTINGS_INFO settings ; 
+
+	memset(&settings,0,sizeof(NLED_SETTINGS_INFO));
+    NLED_COUNT_INFO nci;
+    int nIndex = 0;
+
+    //if(NLedGetDeviceInfo(NLED_COUNT_INFO_ID, (PVOID) &nci))
+    //{
+    //        nIndex = (int)nci.cLeds - 1;
+    //}
+
+    settings.LedNum = nIndex;
+	settings.OffOnBlink= 1;
+	NLedSetDevice(NLED_SETTINGS_INFO_ID, &settings);
+}
+
+void StopVirbate(HWND hwnd, UINT idEvent, UINT_PTR, DWORD )
+{
+	NLED_SETTINGS_INFO settings; 
+
+	memset(&settings,0,sizeof(NLED_SETTINGS_INFO));
+	settings.LedNum= 0; 
+	settings.OffOnBlink= 0;
+	NLedSetDevice(NLED_SETTINGS_INFO_ID, &settings);
+    KillTimer(hwnd, idEvent);
+}
+#endif
+
 CNotify::CNotify(void)
 {
 }
@@ -11,12 +75,22 @@ CNotify::~CNotify(void)
 
 // 播放指定的声音或振动
 // 如果strPath不为空, 则播放声音
-void CNotify::Nodify(CString strPath, int iPeriod, bool bVibe)
+void CNotify::Nodify(HWND hwnd, LPCWSTR strPath, int iPeriod, bool bVibr, UINT Styles)
 {
-    StartViberate(bVibe);
-    // TODO 播放声音指定的长度
+    if(bVibr)
+    {
+#ifdef WIN32_PLATFORM_PSPC
+        StartVirbate();
+        SetTimer(hwnd, TIMER_STOPVIBRATE, iPeriod, StopVirbate);
+#endif
+#ifdef WIN32_PLATFORM_WFSP
+    Vibrate (0, NULL, FALSE, DWORD(iPeriod));
+    //SetTimer(hwnd, TIMER_STOPVIBRATE, iPeriod, StopVib);
+#endif
+    }
+    //播放声音
+    PlaySound (strPath, AfxGetApp()->m_hInstance, Styles | SND_ASYNC);
 
-    StopViberate(bVibe);
 }
 
 void CNotify::StartViberate(bool bVibe)
