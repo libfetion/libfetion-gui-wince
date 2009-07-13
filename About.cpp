6e5bc7d5d19e1f21a0f8e517c44999d5e0df452b
shikun.z <shikun.z@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-07-13 10:01:28 +0000

    上一次传的是没写完的代码,没有办法编译 不好意思.

--- About.cpp
+++ About.cpp
@@ -44,18 +44,20 @@ BOOL CAbout::OnInitDialog()
 		return FALSE;      // 未能创建
 	}
 
-    CString strAboutMsg = _T("关于LibFetion WM版(20090712alpha)\r\n\r\n\
-                             本程序基于LibFetion库编写，了解更多请访问\r\n\
-                             www.libfetion.cn.\r\n\
-                             Copyright @ 2009 DDD(dedodong@163.com) \r\n\
-                             All Rights Reserved.\r\n\r\n\
-                             Windows Mobile 版本由以下人员开发:\r\n\
-                             核心:DDD\r\n\
-                             开发&协调:daviyang35\r\n\
-                             开发&UI:Babylon\r\n\
-                             开发&SP版UI:Eric\r\n\
-                             如有问题欢迎到论坛反馈。";
-
+    CString strAboutMsg = 
+_T("关于LibFetion WM版(20090713alpha)\r\n\r\n\
+本程序基于LibFetion库编写，了解更多请访问\r\n\
+www.libfetion.cn.\r\n\
+Copyright @ 2009 DDD(dedodong@163.com) \r\n\
+All Rights Reserved.\r\n\r\n\
+Windows Mobile 版本由以下人员开发:\r\n\
+核心:\t\tDDD\r\n\
+开发&&协调:\tdaviyang35\r\n\
+开发&&UI:\tBabylon\r\n\
+开发&&SP版UI:\tEric\r\n\
+如有问题欢迎到论坛反馈。");
+    
+    ((CStatic*)this->GetDlgItem(IDC_STATIC_ABOUT))->SetWindowTextW(strAboutMsg);
     return TRUE;  // return TRUE unless you set the focus to a control
     // 异常: OCX 属性页应返回 FALSE
 }
