cc5e2addf4391b8d1c3429066117f41326d1c04b
shikun.z <shikun.z@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-07-13 09:39:18 +0000

    修改SP版添加好友界面 修改ABOUT对话框,将文本从资源中分离到代码

--- trunk/About.cpp
+++ trunk/About.cpp
@@ -27,6 +27,7 @@ void CAbout::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CAbout, CDialog)
+//    ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 
@@ -42,6 +43,20 @@ BOOL CAbout::OnInitDialog()
 		TRACE0("未能创建 CommandBar\n");
 		return FALSE;      // 未能创建
 	}
+
+    CString strAboutMsg = _T("关于LibFetion WM版(20090712alpha)\r\n\r\n\
+                             本程序基于LibFetion库编写，了解更多请访问\r\n\
+                             www.libfetion.cn.\r\n\
+                             Copyright @ 2009 DDD(dedodong@163.com) \r\n\
+                             All Rights Reserved.\r\n\r\n\
+                             Windows Mobile 版本由以下人员开发:\r\n\
+                             核心:DDD\r\n\
+                             开发&协调:daviyang35\r\n\
+                             开发&UI:Babylon\r\n\
+                             开发&SP版UI:Eric\r\n\
+                             如有问题欢迎到论坛反馈。";
+
     return TRUE;  // return TRUE unless you set the focus to a control
     // 异常: OCX 属性页应返回 FALSE
 }
+
