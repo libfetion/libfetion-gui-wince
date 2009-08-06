f74cc8064dfe6bbbf5a4b58cbcca3b3c20c6a1eb
daviyang35 <daviyang35@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-08-06 13:04:42 +0000

    将m_bNoSound改为m_Sound，为TRUE时启用消息提醒 修改主菜单设置，增加清除登陆信息菜单 修改设置中的文字和顺序

--- trunk/resourcesp.h
+++ trunk/resourcesp.h
@@ -209,13 +209,15 @@
 #define IDC_LOGIN_OFFLINE               32870
 #define ID_32871                        32871
 #define IDM_MAIN_SET_ONLINE             32872
+#define ID_32873                        32873
+#define IDM_MAIN_CLEAN                  32874
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        176
-#define _APS_NEXT_COMMAND_VALUE         32873
+#define _APS_NEXT_COMMAND_VALUE         32875
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
