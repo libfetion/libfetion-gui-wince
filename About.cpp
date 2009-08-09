e45a216078534b43bd921671d2c9655818dbd31b
daviyang35 <daviyang35@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-08-09 05:21:13 +0000

    修改About字符串

--- About.cpp
+++ About.cpp
@@ -45,17 +45,17 @@ BOOL CAbout::OnInitDialog()
 	}
 
     CString strAboutMsg = 
-_T("关于LibFetion WM版(第五版修正版)\r\n\r\n\
-本程序基于LibFetion库编写，了解更多请访问\r\n\
-www.libfetion.cn.\r\n\
-Copyright @ 2009 DDD(dedodong@163.com) \r\n\
+_T("关于LibFetion WM版(第六版)\r\n\r\n\
+本程序基于LibFetion库编写，想了解更多\r\n
+请访问:www.libfetion.cn.\r\n\
+Copyright @ 2009 DDD(dedodong@163.com)\r\n\
 All Rights Reserved.\r\n\r\n\
 Windows Mobile 版本由以下人员开发:\r\n\
 \tBabylon\r\n\
-\tdaviyang35\r\n\
-\tEric\r\n\
-\tgladyeti\r\n\
 \txixinxing\r\n\
+\tgladyeti\r\n\
+\tEric\r\n\
+\tdaviyang35\r\n\
 LibFetion库:\t\tDDD\r\n\
 如有问题欢迎到论坛反馈。");
     
