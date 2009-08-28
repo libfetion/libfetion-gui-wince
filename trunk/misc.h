85118ee362600c4eadc8818e76eac6a7928e53ed
daviyang35 <daviyang35@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-08-28 02:42:19 +0000

    去掉不再使用的注册表操作函数

--- trunk/misc.h
+++ trunk/misc.h
@@ -8,27 +8,4 @@
 
 CString GetHttpsWebData(CString Url);
 
-#ifdef __cplusplus 
-extern "C" { 
-#endif 
-
-
-#include <Winreg.h>
-
-#if 0
-long Lib_ReadReg(bool is_id, CString &Buf);
-long Lib_WriteReg(bool is_id, CString Buf);
-#endif
-
-long Lib_ReadReg(CString Key, CString &Buf);
-long Lib_WriteReg(CString Key, CString Buf);
-
-void   OpenRegistry(WCHAR *pFilePath,BOOL nFlag = 0);
-long   ReadRegistry(WCHAR *pRegKey,BYTE *pReadBuf,DWORD nReadLen,DWORD dwType);
-long   WriteRegistry(WCHAR *pRegKey,BYTE *pWriteBuf,DWORD nWriteLen,DWORD dwType);
-void   CloseRegistry(BOOL nCloseFlag = 0);
-
-#ifdef __cplusplus 
-} 
-#endif 
 #endif
