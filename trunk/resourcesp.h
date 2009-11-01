09fb968a443d305c5b93d8a5fb505661685f0af5
gladyeti <gladyeti@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2009-11-01 13:32:52 +0000

    修改SP的资源

--- trunk/resourcesp.h
+++ trunk/resourcesp.h
@@ -189,7 +189,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        182
 #define _APS_NEXT_COMMAND_VALUE         32883
-#define _APS_NEXT_CONTROL_VALUE         1085
+#define _APS_NEXT_CONTROL_VALUE         1086
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
