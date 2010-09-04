13f5113ec5b2ee7ced63221a081b04572cd48279
libfetion <libfetion@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2010-09-04 17:23:09 +0000

    和雪人一起重写GetHttpsWebData()函数，增强错误检测和简化代码

--- misc.cpp
+++ misc.cpp
@@ -18,62 +18,81 @@ int GetHttpsWebData(CString Url, CString *web_data, CString *cookie_data)
 	CString strServerName;
 	CString strObject;
 	DWORD dwServiceType;
-	if (!AfxParseURL(Url, dwServiceType, strServerName, strObject, nPort) || (dwServiceType != AFX_INET_SERVICE_HTTPS))
+	if (!AfxParseURL(Url, dwServiceType, strServerName, strObject, nPort) ||
+		(dwServiceType != AFX_INET_SERVICE_HTTPS))
 	{
-		BOOL bS = FALSE;
 		return -1;
 	}
+
 	session.EnableStatusCallback(TRUE);
 	CHttpConnection* pServer = session.GetHttpConnection(strServerName, nPort);
-	CHttpFile* pFile = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, strObject, NULL, 1, NULL, NULL,	INTERNET_FLAG_SECURE);
+	if (!pServer)
+	{
+		session.Close();
+		return -1;
+	}
+
+	CHttpFile* pFile = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, strObject,
+										NULL, 1, NULL, NULL,	INTERNET_FLAG_SECURE);
+	if (!pFile)
+	{
+		pServer->Close();
+		session.Close();
+		return -1;
+	}
+
 	//end Request 
 	BOOL result = FALSE;
+
 	try
 	{
+		//Get Response
+		CString strGetData;
+		CString strsz;
+
 		result = pFile->SendRequest(strHeaders, (LPVOID)(LPCTSTR)Url, Url.GetLength()); 
+		if (result == FALSE)
+		{
+			pFile->Close();
+			pServer->Close();
+			session.Close();
+			return -1;
+		}
+
+		pFile->SetReadBufferSize(BUFFER_SIZE);
+
+		while (pFile->ReadString(strGetData))
+		{
+			strsz += strGetData;
+		}
+
+		strGetData = ConvertUtf8ToUtf16((LPCSTR)strsz.GetBuffer());
+
+		if (cookie_data)
+		{
+			CString strcookie;
+			DWORD dw = 0;
+			pFile->QueryInfo(HTTP_QUERY_SET_COOKIE, strcookie, &dw);
+			*cookie_data = strcookie;
+		}
+
+		if (web_data)
+		{
+			*web_data = strGetData;
+		}
+
 	}
+
 	catch(CInternetException * pException)
 	{
 		pException->m_dwError;
 		pException->Delete();
+		return -1;
 	}
-	//Get Response
-	CString strGetData;
-	TCHAR* szWEBPage = new TCHAR[BUFFER_SIZE+1];
-	if(szWEBPage)
-	{
-		szWEBPage[0] = L'\0';
-		TCHAR* sz     = new TCHAR[BUFFER_SIZE+1];
-		TCHAR* szwBuf = new TCHAR[(BUFFER_SIZE+1)*2];
-		sz[0] = L'\0';
-		szwBuf[0] = L'\0';
-		DWORD n = 0;
-		printf("pFile:%d\n",pFile->GetLength());
-		pFile->SetReadBufferSize(BUFFER_SIZE*2);
-		while (pFile->ReadString(sz, BUFFER_SIZE))//每次一行数据
-		{
-			mbstowcs(szwBuf, (char*)sz, BUFFER_SIZE+1);
-			n += _tcslen(szwBuf);
-			if(n >= BUFFER_SIZE)
-				break;
-			_tcscat(szWEBPage, szwBuf);
-		}
-		delete [] sz;
-		sz = NULL;
-		delete [] szwBuf;
-		szwBuf = NULL;
-		
-		CString strcookie;
-		DWORD dw = 0;
-		pFile->QueryInfo(HTTP_QUERY_SET_COOKIE, strcookie, &dw);
-		if (cookie_data)
-			*cookie_data = strcookie;
-	}
-	strGetData = szWEBPage;
-	delete [] szWEBPage;
-	szWEBPage = NULL;
 
-	if (web_data)
-		*web_data = strGetData;
+
+	pFile->Close();
+	pServer->Close();
+	session.Close();
 	return 0;
 }
