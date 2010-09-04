716b3607a3d6695ced6675632323bb21b5661307
libfetion <libfetion@8f3339bc-7e71-11dd-a943-c76e4c82ec5c>
2010-09-04 04:12:00 +0000

    lib:在link_openssl.cpp里实现一些openssl静态库需要的函数，使得链接成功.

---
+++ trunk/link_openssl.cpp
@@ -0,0 +1,32 @@
+
+#include "stdafx.h"
+#include "WMLF.h"
+#include "link_openssl.h"
+
+time_t __cdecl time(time_t * t32)
+{
+	__time64_t t64;
+	time_t r = (time_t)_time64(&t64);
+	if (t32)
+		*t32 = (time_t)t64;
+	return r;
+}
+
+
+
+char* __cdecl strerror(int errnum)
+{
+	return "Unknown Error";
+}
+
+void __cdecl abort(void)
+{
+	int * i = NULL;
+	*i = 1;
+	return;
+}
+
+char * __cdecl getenv(char *envvar)
+{
+	return NULL;
+}
